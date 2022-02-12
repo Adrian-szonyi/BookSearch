0d56f3fdbf5a18f629591f1c393672e1a86aed14
unknown <aszonyi49@gmail.com>
2022-02-12 22:36:34 +1100

    fixed login and signup logic

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { ADD_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
 
-import { createUser } from '../utils/API';
-import Auth from '../utils/auth';
 
 const SignupForm = () => {
   // set initial form state
@@ -16,9 +16,10 @@ const SignupForm = () => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   };
-
+  const [addUser] = useMutation(ADD_USER);
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+    
 
     // check if form has everything (as per react-bootstrap docs)
     const form = event.currentTarget;
@@ -28,15 +29,11 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const { data } = await addUser({
+        variables: { ...userFormData },
+      });
+      console.log(data);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
